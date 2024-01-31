8517764ae7373fc8a1ebe83a51bbba1f008c844f
weijiazhang <weijiazhang@microsoft.com>
2024-01-31 17:11:52 +0800

    First commit.

---
+++ src/app/layout.tsx
@@ -0,0 +1,78 @@
+import Footer from "@/app/_components/footer";
+import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
+import type { Metadata } from "next";
+import { Noto_Sans, Noto_Serif, Noto_Sans_Mono } from "next/font/google";
+
+import "./globals.css";
+
+const sans = Noto_Sans({
+  subsets: ["latin", "latin-ext"],
+  display: "swap",
+  variable: "--font-noto-sans",
+});
+const serif = Noto_Serif({
+  subsets: ["latin", "latin-ext"],
+  display: "swap",
+  variable: "--font-noto-serif",
+});
+const mono = Noto_Sans_Mono({
+  subsets: ["latin", "latin-ext"],
+  display: "swap",
+  variable: "--font-noto-sans-mono",
+});
+
+export const metadata: Metadata = {
+  title: `Next.js Blog Example with ${CMS_NAME}`,
+  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  openGraph: {
+    images: [HOME_OG_IMAGE_URL],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="zh">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#000000"
+        />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta
+          name="msapplication-config"
+          content="/favicon/browserconfig.xml"
+        />
+        <meta name="theme-color" content="#000" />
+        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      </head>
+      <body className={`${sans.variable} ${serif.variable} ${mono.variable}`}>
+        <div className="min-h-screen font-sans">{children}</div>
+        <Footer />
+      </body>
+    </html>
+  );
+}
