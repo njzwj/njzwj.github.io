d29d29d6241205252345395800ba034bf9d55a9a
randc0degen <njzwj2007@126.com>
2024-06-24 02:51:22 +0800

    update figure render and image for new post

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,17 @@
-import { remark } from "remark";
-import html from "remark-html";
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkGfm from 'remark-gfm';
+import remarkRehype from 'remark-rehype';
+import rehypeFigure from '@microflash/rehype-figure';
+import rehypeStringify from 'rehype-stringify';
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypeFigure)
+    .use(rehypeStringify)
+    .process(markdown);
   return result.toString();
 }
