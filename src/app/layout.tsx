1f33c7e404d65a8ac4e0fee2ec29d4da65e5422b
Zhang Weijia <njzwj2007@126.com>
2025-02-07 15:32:02 +0800

    Update layout.tsx
    Use noto serif instead.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import "./globals.css";
-import { Noto_Sans, Noto_Serif, Noto_Sans_Mono, Noto_Serif_SC } from "next/font/google";
+import { Noto_Sans, Noto_Serif, Noto_Sans_Mono } from "next/font/google";
 import Providers from "./providers";
 
-const serif = Noto_Serif_SC({
-  subsets: ["latin", "simplified-han", "katakana", "hiragana", "emoji-symbols", "hangul", "greek"],
+const serif = Noto_Serif({
+  subsets: ["latin", "latin-ext", "simplified-han", "katakana", "hiragana", "emoji-symbols", "hangul", "greek"],
   display: "swap",
   variable: "--font-noto-serif",
 });
