e09bfef81891a77afb15ac850c31edfadb33680f
njzwj <njzwj2007@126.com>
2025-02-07 19:07:09 +0800

    Update page style and cc.

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,11 +1,41 @@
+import { Divider } from "@nextui-org/react";
+
 import Body from "./body"
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className="mt-32">
+      <Divider />
       <Body>
-        <div className="pt-10 pb-10 flex flex-row justify-center">
-          <h3 className="text-sm text-gray-600">
+        <div className="pt-10 pb-32 flex flex-col justify-center items-center">
+          <p className="text-sm text-gray-600">
+            This work by <span property="cc:attributionName">njzwj</span> is licensed under 
+            {" "}
+            <a 
+              href="https://creativecommons.org/licenses/by-nc/4.0/?ref=chooser-v1" 
+              target="_blank" 
+              rel="license noopener noreferrer" 
+              className="text-green-600 hover:bg-green-600 hover:text-white duration-200 transition-colors inline-flex items-center" 
+            >
+              CC BY-NC 4.0
+              <img 
+              style={{ height: "22px", marginLeft: "3px", verticalAlign: "middle" }} 
+              src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" 
+              alt=""
+              />
+              <img 
+              style={{ height: "22px", marginLeft: "3px", verticalAlign: "middle" }} 
+              src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" 
+              alt=""
+              />
+              <img 
+              style={{ height: "22px", marginLeft: "3px", verticalAlign: "middle" }} 
+              src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1" 
+              alt=""
+              />
+            </a>
+          </p>
+          <h3 className="text-sm text-gray-600 pt-4">
             Made with love at
             {" "}
             <a
--- src/app/components/post-list.tsx
+++ src/app/components/post-list.tsx
@@ -27,9 +27,8 @@ function PostCard (
       isFooterBlurred
       isPressable
       onClick={() => window.location.href = `/posts/${slug}`}
-      className="min-h-[300px] shadow-lg"
     >
-      <CardFooter className="absolute z-10 bottom-0 flex-col !items-start border-t-1 bg-white/60 border-zinc-100/50 dark:bg-black/40 dark:border-default-300">
+      {/* <CardFooter className="absolute z-10 bottom-0 flex-col !items-start border-t-1 bg-white/60 border-zinc-100/50 dark:bg-black/40 dark:border-default-300">
         <h4 className="text-md font-bold text-left">{title}</h4>
         <p className="text-sm text-default-500">
           {excerpt}
@@ -37,20 +36,26 @@ function PostCard (
         <p className="text-small text-default-500">
           {new Date(date).toLocaleDateString("en-US")}
         </p>
-      </CardFooter>
+      </CardFooter> */}
 
-      <Image
-        removeWrapper
-        className="z-0 w-full h-full object-cover"
-        src={coverImagePath}
-      />
+      <CardBody className="overflow-visible p-2">
+        <Image
+          alt={title}
+          removeWrapper
+          className="object-cover rounded-xl mb-4"
+          src={coverImagePath}
+        />
+        <small className="text-sm text-default-500">{new Date(date).toLocaleDateString("en-US")}</small>
+        <h4 className="font-bold text-lg text-left">{title}</h4>
+        <small className="text-sm text-default-500">{excerpt}</small>
+      </CardBody>
     </Card>
   );
 }
 
 const PostList = ({ posts }: Props) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+    <div className="grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 gap-6">
       {posts.map((post) => (
         <PostCard
           key={post.slug}
